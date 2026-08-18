5bc232f41b38f31ea6122ae307611845aadc0eff
jjj2221 <2592406434@qq.com>
2026-08-18 19:09:47 +0800

    Create mpconfigboard.mk
    Signed-off-by: jjj2221 <2592406434@qq.com>

---
+++ ports/stm32/boards/BLUE_PILL_F103C8/mpconfigboard.mk
@@ -0,0 +1,9 @@
+# Board configuration for STM32F103C8T6 Blue Pill
+
+LD_FILE = boards/stm32f103c8.ld
+MCU_SERIES = stm32f1
+CMSIS_MCU = STM32F103C8
+
+# 如果需要，可以添加其他选项，例如：
+# MICROPY_FLOAT_IMPL = double   # 默认是双精度，可能占用较大，但可接受
+# MICROPY_PY_SYS_STDFILES = 1
